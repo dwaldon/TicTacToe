54028eb49544c3caa83dc682db863b824329fd77
dwaldon <devinwaldon5@gmail.com>
2020-05-20 22:38:44 -0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace TicTacToe
+{
+    partial class TicTacToe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TicTacToe));
+            this.gameLbl = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gameLbl
+            // 
+            this.gameLbl.AutoSize = true;
+            this.gameLbl.Location = new System.Drawing.Point(324, 9);
+            this.gameLbl.Name = "gameLbl";
+            this.gameLbl.Size = new System.Drawing.Size(68, 20);
+            this.gameLbl.TabIndex = 1;
+            this.gameLbl.Text = "O\'s Turn";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.resetToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(778, 36);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem});
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(70, 30);
+            this.resetToolStripMenuItem.Text = "Reset";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitGameToolStripMenuItem});
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(55, 30);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // exitGameToolStripMenuItem
+            // 
+            this.exitGameToolStripMenuItem.Name = "exitGameToolStripMenuItem";
+            this.exitGameToolStripMenuItem.Size = new System.Drawing.Size(270, 34);
+            this.exitGameToolStripMenuItem.Text = "Exit Game";
+            this.exitGameToolStripMenuItem.Click += new System.EventHandler(this.exitGameToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(0, 78);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(750, 750);
+            this.panel1.TabIndex = 3;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // TicTacToe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 844);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.gameLbl);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimumSize = new System.Drawing.Size(800, 900);
+            this.Name = "TicTacToe";
+            this.Text = "Tic Tac Toe";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label gameLbl;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitGameToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,347 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class TicTacToe : Form
+    {
+        public TicTacToe()
+        {
+            InitializeComponent();
+        }
+        public Shape shp;
+        public Game game;
+        
+        public class Game : TicTacToe
+        {
+            
+            public PlaceHolder[,] grids = new PlaceHolder[3,3];
+            //private Rectangle[,] coords = new Rectangle[3,3];
+            public const int X = 0;
+            public const int O = 1;
+            public const int B = 2;
+            public int pMoves = 0;
+            public int pTurn = O;
+            public int xWins = 0;
+            public int oWins = 0;
+
+            //Checking for a tie
+            bool checkForTie(int turns)
+            {
+                bool tie = false;
+                for(int row = 0; row < 3; row++)
+                {
+                    for(int col = 0; col < 3; col++)
+                    {
+                        if(turns == 9)
+                        {
+                            return true;
+                        }
+                    }
+                }
+                return tie;
+            }
+            public int getPTurn()
+            {
+                return pTurn;
+            }
+            public int getXWins()
+            {
+                return xWins;
+            }
+            public int getOWins()
+            {
+                return oWins;
+            }
+            public void Board()
+            {
+                for(int i = 0; i < 3; i++)
+                {
+                    for(int j = 0; j < 3; j++)
+                    {
+                        //coords[i, j] = new Rectangle(i * 167, j * 167, 167, 167);
+                        grids[i, j] = new PlaceHolder();
+                        grids[i, j].setVal(B);
+                        grids[i, j].setLoc(new Point(i, j));
+                    }
+                }
+            }
+            //Keeping track of player mouse clicks
+            public void playerClick(Point p)
+            {
+                int x = 0;
+                int y = 0;
+
+                if(p.X < 167)
+                {
+                    x = 0;
+                }
+                else if(p.X > 167 && p.X < 334)
+                {
+                    x = 1;
+                }
+                else if(p.X > 334)
+                {
+                    x = 2;
+                }
+
+
+                if (p.Y < 167)
+                {
+                    y = 0;
+                }
+                else if (p.Y > 167 && p.Y < 334)
+                {
+                    y = 1;
+                }
+                else if (p.Y > 334)
+                {
+                    y = 2;
+                }
+                pMoves++;
+
+                if (pMoves % 2 == 0)
+                {
+                    gameLbl.Text = "X's Turn!";
+                    Shape.DrawX(new Point(x, y));
+                    grids[x, y].setVal(X);
+                    if (Win())
+                    {
+                        MessageBox.Show("X's have won!"); 
+                        resetGame();
+                        Shape.Draw();
+                        xWins++;
+                        pMoves = 0;
+                    }
+                   else if (checkForTie(pMoves))
+                    {
+                        MessageBox.Show("It was a tie!");
+                        resetGame();
+                        Shape.Draw();
+                        pMoves = 0;
+                    }
+                    pTurn = O;
+                }
+                else
+                {
+                    gameLbl.Text = "O's Turn!";
+                    Shape.DrawO(new Point(x, y));
+                    grids[x, y].setVal(O);
+                    if (Win())
+                    {
+                        MessageBox.Show("O's have won!");
+                        resetGame();
+                        Shape.Draw();
+                        oWins++;
+                        pMoves = 0;
+                    }
+                    else if (checkForTie(pMoves))
+                    {
+                        MessageBox.Show("It was a tie!");
+                        resetGame();
+                        Shape.Draw();
+                        pMoves = 0;
+                    }
+                    pTurn = X;
+                }
+            
+            }
+            //Checking for winner
+            public bool Win()
+            {
+                bool Won = false;
+
+                for(int x = 0; x < 3; x++)
+                {
+                    if(grids[x, 0].getVal() == X && grids[x, 1].getVal() == X && grids[x, 2].getVal() == X)
+                    {
+                        return true;
+                    }
+                    if (grids[x, 0].getVal() == O && grids[x, 1].getVal() == O && grids[x, 2].getVal() == O)
+                    {
+                        return true;
+                    }
+                    switch (x)
+                    {
+                        case 0:
+                            if (grids[x, 0].getVal() == X && grids[x + 1, 1].getVal() == X && grids[x + 2, 2].getVal() == X)
+                            {
+                                return true;
+                            }
+                            if (grids[x, 0].getVal() == O && grids[x + 1, 1].getVal() == O && grids[x + 2, 2].getVal() == O)
+                            {
+                                return true;
+                            }
+                                break;
+                        case 2:
+                            if (grids[x, 0].getVal() == X && grids[x - 1, 1].getVal() == X && grids[x - 2, 2].getVal() == X)
+                            {
+                                return true;
+                            }
+                            if (grids[x, 0].getVal() == O && grids[x - 1, 1].getVal() == O && grids[x - 2, 2].getVal() == O)
+                            {
+                                return true;
+                            }
+                                break;
+                    }
+                }
+                for(int y = 0; y < 3; y++)
+                {
+                    if (grids[0, y].getVal() == X && grids[1, y].getVal() == X && grids[2, y].getVal() == X)
+                    {
+                        return true;
+                    }
+                    if (grids[0, y].getVal() == O && grids[1, y].getVal() == O && grids[2, y].getVal() == O)
+                    {
+                        return true;
+                    }
+                }
+                return Won;
+            }
+            //Resetting the game board
+            public void resetGame()
+            {
+                grids = new PlaceHolder[3, 3];
+                Board();
+            }
+        }
+        
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Shape.graphics = CreateGraphics();
+        }
+      public class Shape
+        {
+            private static Graphics graphics;
+            public Shape(Graphics g)
+            {
+                graphics = g;
+                Draw();
+            }
+            //Drawing the game display
+            public static void Draw()
+            {
+                //Drawing the playing field
+                Brush backG = new SolidBrush(Color.White);
+                Pen line = new Pen(Color.Black, 5);
+                graphics.FillRectangle(backG, new Rectangle(0, 0, 500, 600));
+                graphics.DrawLine(line, new Point(167, 0), new Point(167, 600));
+                graphics.DrawLine(line, new Point(334, 0), new Point(334, 600));
+                graphics.DrawLine(line, new Point(0, 167), new Point(500, 167));
+                graphics.DrawLine(line, new Point(0, 334), new Point(500, 334));
+            }
+            
+            public static void DrawX(Point p)
+            {
+                //Creating the X marker
+                Pen X = new Pen(Color.Red, 5);
+                int XP = p.X * 167;
+                int YP = p.Y * 167;
+                //if(Game.grids[,] )
+                graphics.DrawLine(X, XP+10, YP+10, XP + 140, YP + 140);
+                graphics.DrawLine(X, XP + 140, YP+10, XP+10, YP + 140);
+            }
+            public static void DrawO(Point p)
+            {
+                //Creating the O marker
+                Pen O = new Pen(Color.Blue, 5);
+                int XP = p.X * 167;
+                int YP = p.Y * 167;
+
+                graphics.DrawEllipse(O, XP+8, YP+8, 140, 140);
+            }
+        }
+
+        public class PlaceHolder
+        {
+            private Point loc;
+            private int val = Game.B;
+            public void setLoc(Point point)
+            {
+                loc = point;
+            }
+            public Point getLoc()
+            {
+                return loc;
+            }
+            public void setVal(int x)
+            {
+                val = x;
+            }
+            public int getVal()
+            {
+                return val;
+            }
+        }
+        
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        { 
+   
+        }
+        
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+        
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics pass = panel1.CreateGraphics();
+            shp = new Shape(pass);
+            game = new Game();
+            game.Board();
+
+            changeLabel();
+        }
+
+        private void panel1_Click(object sender, EventArgs e)
+        {
+            Point mouse = Cursor.Position;
+            mouse = panel1.PointToClient(mouse);
+            game.playerClick(mouse);
+            changeLabel();
+        }
+        //closing game menu strip button
+        private void exitGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        //reset game menu strip button
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            game.resetGame();
+            Shape.Draw();
+        }
+        
+        //Displaying player's turn and amount of wins per player
+        public void changeLabel()
+        {
+            string lText = "";
+            if(game.getPTurn() == Game.X)
+            {
+                lText = "X's Turn!\n";
+            }
+            else
+            {
+                lText = "O's Turn!\n";
+            }
+            lText += "X has won " + game.getXWins() + " times. \nO has won " + game.getOWins() + " times.";
+            gameLbl.Text = lText;
+        }
+    }
+    
+}
